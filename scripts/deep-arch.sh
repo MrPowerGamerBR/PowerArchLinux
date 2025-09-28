8d906663d5c4155cea40bebf70b83d98863c2149
MrPowerGamerBR <9496359+MrPowerGamerBR@users.noreply.github.com>
2025-09-28 18:22:00 -0300

    Update deep-arch.sh

--- scripts/deep-arch.sh
+++ scripts/deep-arch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # PowerArchLinux - Deep Arch Setup
-wget -O bootstrap-stage1.sh https://raw.githubusercontent.com/MrPowerGamerBR/PowerArchLinux/refs/heads/main/bootstrap-stage1.sh
+curl -L -o bootstrap-stage1.sh https://raw.githubusercontent.com/MrPowerGamerBR/PowerArchLinux/refs/heads/main/bootstrap-stage1.sh
 bash bootstrap-stage1.sh
