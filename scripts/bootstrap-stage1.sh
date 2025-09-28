e958c9a371e25e84bf88ea9da70c8ea6d4df91eb
MrPowerGamerBR <9496359+MrPowerGamerBR@users.noreply.github.com>
2025-09-28 18:50:38 -0300

    Update bootstrap-stage1.sh

--- scripts/bootstrap-stage1.sh
+++ scripts/bootstrap-stage1.sh
@@ -5,7 +5,7 @@ mount --mkdir /dev/nvme0n1p1 /mnt/efi
 reflector --country Brazil --protocol http,https --sort rate --fastest 2 --save /etc/pacman.d/mirrorlist
 # Apenas o essencial é instalado aqui, é um "bootstrap" para primeiro instalar as coisas realmente essenciais para depois instalar as coisas menos essenciais
 # Assim é até melhor, pois evita você ficar muito tempo esperando instalar todas as packages enquanto você olha para um terminal
-pacstrap -K /mnt base base-devel linux linux-firmware pacman-contrib amd-ucode nano networkmanager git nvidia-open grub efibootmgr vi vim sudo curl wget zip unzip less rsync firefox plasma-meta kde-system sddm konsole kwalletmanager noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono
+pacstrap -K /mnt base base-devel linux linux-firmware pacman-contrib amd-ucode nano networkmanager git nvidia-open grub efibootmgr vi vim sudo curl wget zip unzip less rsync firefox plasma-meta kde-system sddm konsole kwalletmanager reflector tailscale fastfetch code discord flatpak flatpak-kcm htop ntfs-3g qbittorrent dosfstools noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono
 genfstab -U /mnt >> /mnt/etc/fstab
 curl -L -o /mnt/bootstrap-stage2.sh https://raw.githubusercontent.com/MrPowerGamerBR/PowerArchLinux/refs/heads/main/scripts/bootstrap-stage2.sh
 echo "Rode o script /bootstrap-stage2.sh para continuar :3"
