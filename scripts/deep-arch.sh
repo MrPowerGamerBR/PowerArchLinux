4ab38050a1b23c39ec71c2e2610432863c34d777
MrPowerGamerBR <9496359+MrPowerGamerBR@users.noreply.github.com>
2025-09-28 18:12:51 -0300

    Create deep-arch.sh

---
+++ scripts/deep-arch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# PowerArchLinux - Deep Arch Setup
+wget -O bootstrap-stage1.sh https://raw.githubusercontent.com/MrPowerGamerBR/PowerArchLinux/refs/heads/main/bootstrap-stage1.sh
+bash bootstrap-stage1.sh
