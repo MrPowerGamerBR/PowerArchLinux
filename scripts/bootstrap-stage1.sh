6ac303ec23bbe1e7b05e503d352278b3e452e4db
MrPowerGamerBR <9496359+MrPowerGamerBR@users.noreply.github.com>
2025-09-28 18:28:09 -0300

    Update bootstrap-stage1.sh

--- scripts/bootstrap-stage1.sh
+++ scripts/bootstrap-stage1.sh
@@ -7,6 +7,6 @@ reflector --country Brazil --protocol http,https --sort rate --fastest 2 --save 
 # Assim é até melhor, pois evita você ficar muito tempo esperando instalar todas as packages enquanto você olha para um terminal
 pacstrap -K /mnt base base-devel linux linux-firmware pacman-contrib amd-ucode nano networkmanager git nvidia-open grub efibootmgr vi vim sudo curl wget zip unzip less rsync firefox plasma-meta kde-system sddm konsole kwalletmanager noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono
 genfstab -U /mnt >> /mnt/etc/fstab
-curl -L -o /mnt/bootstrap-stage2.sh https://raw.githubusercontent.com/MrPowerGamerBR/PowerArchLinux/refs/heads/main/bootstrap-stage2.sh
+curl -L -o /mnt/bootstrap-stage2.sh https://raw.githubusercontent.com/MrPowerGamerBR/PowerArchLinux/refs/heads/main/scripts/bootstrap-stage2.sh
 echo "Rode o script /bootstrap-stage2.sh para continuar :3"
 arch-chroot /mnt
