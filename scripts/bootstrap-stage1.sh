90f06b3796fb0700fb2349a9ea30e7f1d91d0896
MrPowerGamerBR <9496359+MrPowerGamerBR@users.noreply.github.com>
2025-12-28 18:10:55 -0300

    Update bootstrap-stage1.sh

--- scripts/bootstrap-stage1.sh
+++ scripts/bootstrap-stage1.sh
@@ -15,9 +15,7 @@ rm /mnt/boot/vmlinuz-linux
 # Assim é até melhor, pois evita você ficar muito tempo esperando instalar todas as packages enquanto você olha para um terminal
 echo "Instalando pacotes (bootstrap)..."
 
-# PACKAGE DESCRIPTIONS:
-# dolphin-plugins: Adds useful features to Dolphin (like right click on a ISO file -> Mount)
-# ark: Compressor/Decompressor
+# Recommended KDE packages (not EVERYTHING is actually useful, but it is a good pointer): https://community.kde.org/Distributions/Packaging_Recommendations
 pacstrap -K /mnt base base-devel linux linux-firmware pacman-contrib amd-ucode nano networkmanager git efibootmgr vi vim sudo curl wget zip unzip less rsync firefox plasma-meta kde-system sddm konsole kwalletmanager fish reflector pkgstats screen tailscale fastfetch discord flatpak flatpak-kcm htop ntfs-3g qbittorrent dosfstools openssh ksshaskpass noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono \
     # Has "hostname" command
     inetutils \
@@ -27,6 +25,24 @@ pacstrap -K /mnt base base-devel linux linux-firmware pacman-contrib amd-ucode n
     mtr \
     # Adds useful features to Dolphin (like right click on a ISO file -> Mount)
     dolphin-plugins \
+    # Video thumbnails in Dolphin
+    ffmpegthumbs \
+    # KDE Connect
+    kdeconnect \
+    # PDF thumbnails & others in Dolphin
+    kdegraphics-thumbnailers \
+    # VLC + codec plugins
+    vlc vlc-plugins-all \
+    # Use VLC as a backend for Phonon
+    phonon-vlc \
+    # KDE recommends this for tray icons
+    libappindicator \
+    # Adds supports for webp & others, allows Dolphin to display thumbnails for these formats
+    qt-imageformats \
+    # Printer things
+    system-config-printer-applet system-config-printer-dbus-service \
+    # Global crash handler for drkonqi
+    systemd-coredumpd \
     # Compressor/Decompressor that integrates nicely in Dolphin
     ark \
     # Vulkan Driver for AMD Radeon GPUs
